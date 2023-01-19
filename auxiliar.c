fef17bf814cee5c1884d89a9140e5f806b54d8d0
LEONARDO DA SILVA CAMARGO <lsc20@inf.ufpr.br>
2023-01-19 11:19:57 -0300

    adicionada a funcao ABS que estava na biblioteca Edo antes e adicionada a funcao de timestamp

--- auxiliar.c
+++ auxiliar.c
@@ -15,3 +15,32 @@ void imprime_vetor(double *vetor, int n)
     }
     printf("\n");
 }
+/*!
+    \brief zera um vetor de double
+
+    \param vetor vetor de double
+    \param n tamanho do vetor 
+*/
+void zera_vetor(double *vetor, int n)
+{
+    for (int i = 0; i < n; i++)
+        vetor[i] = 0.0;
+    
+}
+
+/*  Retorna tempo em milisegundos desde EPOCH
+
+    Forma de uso:
+ 
+    double tempo;
+    tempo = timestamp();
+    <trecho de programa do qual se deseja medir tempo>
+    tempo = timestamp() - tempo;
+*/
+
+double timestamp (void)
+{
+  struct timespec tp;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
+  return ( (double) tp.tv_sec*1.0e3 + (double) tp.tv_nsec*1.0e-6 );
+}
--- auxiliar.h
+++ auxiliar.h
@@ -2,5 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <float.h>
+#include <time.h>
+#include <sys/time.h>
+
+// Valor absoluto de um número. Alternativa ao uso da função 'fabs()'
+#define ABS(num)  ((num) < 0.0 ? -(num) : (num))
+
 
 void imprime_vetor(double *vetor, int n);
+void zera_vetor(double *vetor, int n);
+double timestamp(void);
+
+
