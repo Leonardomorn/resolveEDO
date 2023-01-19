9a15f0b5b7b3c05e51f4281a9dba04bfe9df8724
LEONARDO DA SILVA CAMARGO <lsc20@inf.ufpr.br>
2023-01-19 11:20:15 -0300

    ajustadas as chamadas de função

--- resolveEDO.c
+++ resolveEDO.c
@@ -7,19 +7,59 @@
 int main ()
 {
     Edo* edeoq; SL_Tridiag* sl;
-    int n = 5;
+    int n;
     double norma; 
-    double *x = malloc( n  * sizeof(double));
+    double *X;
+    double tempo;
     edeoq = malloc (sizeof(Edo)); sl = malloc (sizeof(SL_Tridiag));
+    
+    //resolve para número de passos igual a 5
+    n = 5;
+    X = malloc( n  * sizeof(double));
     inicializa_EDO(edeoq, n);
-    //imprime_EDO (edeoq);
     aloca_tri_diagonal(edeoq, sl);
     gera_tri_diagonal(edeoq, sl);
     imprime_SL(edeoq, sl);
-    gaussSeidel(sl->D, sl->Di, sl->Ds, sl->B, x, n, &norma, sl);
     
-    printf("o vetor solução para n = 5 é:\n");
-    imprime_vetor(x,n);
-    printf("a norma n = 5 é:%f \n", norma);
 
+    gaussSeidel_vetor(sl->D, sl->Di, sl->Ds, sl->B, X, n, &norma, sl, &tempo);
+
+    imprime_resumo_gaussSeidel(GS_COM_VETOR, n, tempo, norma);
+
+    imprime_EDO(edeoq);
+    gaussSeidel_direto(edeoq, sl, X, &norma, &tempo);
+    imprime_resumo_gaussSeidel(GS_SEM_VETOR, n, tempo, norma);
+
+    libera_tri_diagonal(edeoq, sl);
+    free(X);
+    //resolve para número de passos igual a 10
+    n = 10;
+    X = malloc( n  * sizeof(double));
+    ajusta_n_EDO(edeoq, n);
+    aloca_tri_diagonal(edeoq,sl);
+    gera_tri_diagonal(edeoq,sl);
+    imprime_SL(edeoq, sl);
+    
+    gaussSeidel_vetor(sl->D, sl->Di, sl->Ds, sl->B, X, n, &norma, sl, &tempo);
+    imprime_resumo_gaussSeidel(GS_COM_VETOR, n, tempo, norma);
+    
+    gaussSeidel_direto(edeoq, sl, X, &norma, &tempo);
+    imprime_resumo_gaussSeidel(GS_SEM_VETOR, n, tempo, norma);
+
+    libera_tri_diagonal(edeoq, sl);
+    free(X);
+    //resolve para número de passos igual a 100
+    n = 100;
+    ajusta_n_EDO(edeoq, n);
+    aloca_tri_diagonal(edeoq,sl);
+    gera_tri_diagonal(edeoq,sl);
+    imprime_SL(edeoq, sl);
+    
+    gaussSeidel_vetor(sl->D, sl->Di, sl->Ds, sl->B, X, n, &norma, sl, &tempo);
+    imprime_resumo_gaussSeidel(GS_COM_VETOR, n, tempo, norma);
+    
+    gaussSeidel_direto(edeoq, sl, X, &norma, &tempo);
+    imprime_resumo_gaussSeidel(GS_SEM_VETOR, n, tempo, norma);
+
+    libera_tri_diagonal(edeoq, sl);
 }
